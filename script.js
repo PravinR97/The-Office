5c9b4ba4eaec23514ec76f78e06cf61f5d129800
Pravin Raut <Pravinrautt97@gmail.com>
2026-02-04 23:32:33 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,47 @@
-let currentStep = 0;
-const steps = document.querySelectorAll('.step');
+let step = 0;
 
-function nextStep() {
-  steps[currentStep].classList.remove('active');
-  currentStep++;
-  steps[currentStep].classList.add('active');
+const steps = [
+  {
+    image: "images/michael_1.jpg",
+    text: "Michael believes every story needs heart. Do you agree?"
+  },
+  {
+    image: "images/dwight_1.jpg",
+    text: "Dwight demands commitment. Are you serious?"
+  },
+  {
+    image: "images/dwight_cry_1.jpg",
+    text: "Dwight cries only once a year. Continue?"
+  },
+  {
+    image: "images/jim_1.jpg",
+    text: "Jim smirks. You made it this far."
+  },
+  {
+    image: "images/pam_1.jpg",
+    text: "Pam wonders… should this story continue someday?"
+  }
+];
+
+function answerYes() {
+  step++;
+  if (step < steps.length) {
+    updateContent();
+  } else {
+    endStory();
+  }
+}
+
+function answerNo() {
+  alert("No is not an option here 🙂");
+}
+
+function updateContent() {
+  document.getElementById("storyImage").src = steps[step].image;
+  document.getElementById("storyText").innerText = steps[step].text;
+}
+
+function endStory() {
+  document.getElementById("storyText").innerText =
+    "This story stays here… unless someday you choose to continue it 🤍";
 }
